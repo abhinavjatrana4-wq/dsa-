4b8a3dd46f1b56100b738ae007cf777e15300f4c
Abhinav Chaudhary <abhinavjatrana4@gmail.com>
2026-07-06 11:39:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,11 +3,11 @@ public:
     bool isPalindrome(int x) {
         
       if(x < 0 || (x % 10 == 0 && x != 0  )){
-        return false ;
-      }
+        return false ; }
       int rev = 0 ;
       while(x > rev ){
-        rev = rev* 10 +  x % 10 ;
+        rev = rev* 10 +  
+        (x % 10) ;
         x= x /10 ;
       }
       return x  == rev || x == rev /10;
